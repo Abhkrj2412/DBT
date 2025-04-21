664eed4201fc108883561ad0082c4b7905febbab
abhishek.raj@dynpro.com <abhishek.raj@dynpro.com>
2025-04-21 19:42:07 +0000

    ffff

--- macros/debug_query.sql
+++ macros/debug_query.sql
@@ -3,7 +3,7 @@
         BRAND,
         "Revenue Stream",
         ROUND(SUM(SALES), 2) AS total_sales,
-        ROUND(SUM(Gross_Profit), 2) AS total_gross_profit,
+        ROUND(SUM("Gross_Profit"), 2) AS total_gross_profit,
         ROUND(AVG("Gross Profit %"), 2) AS avg_gross_profit_pct
     FROM BOOTCAMP.MTS_RAW.MTS_SALES_DATA
     GROUP BY BRAND, "Revenue Stream"
